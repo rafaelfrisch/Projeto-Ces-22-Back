93cee431eeb75bd237c3b04976c32c653c0bf38b
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-05-11 21:40:34 -0300

    database config

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'dbces22'),
+        'USER': os.environ.get('DB_USER', 'ces22'),
+        'PASSWORD': os.environ.get('DB_PASS', 'projeto'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
