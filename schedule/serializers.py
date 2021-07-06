fb4519544b4d72817bbff30799fb9c9d6a997053
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-07-06 01:55:09 -0300

    Changed Schedule serializer

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Schedule
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    doctor_name = serializers.CharField(source='doctor.name')
+
     class Meta:
         model = Schedule
-        fields = '__all__'
+        fields = ['initial_time', 'final_time', 'doctor', 'doctor_name']
